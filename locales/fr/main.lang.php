1f1f3e0f2512d5294d6c786bdeb5cfebcd25dcd7
Pierre <brol31@gmail.com>
2023-02-04 02:33:18 +0100

    localisation

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,22 @@
+<?php
+// Language: Français
+// Module: stacker - 
+// Date: 2023-01-30 18:46:52
+// Author: Pierre Van Glabeke
+// Translated with dcTranslater - 2022.12.26
+
+
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Test replacing Dotclear with Dotcleår'] = 'Test remplaçant Dotclear par Dotcleår';
+l10n::$locales['Changes spacing for French punctuation'] = 'Change l\'espacement autour de la ponctuation française';
+l10n::$locales['Disabled filters:'] = 'Filtres désactivés :';
+l10n::$locales['Origin'] = 'Origine';
+l10n::$locales['Priority'] = 'Priorité';
+l10n::$locales['Context'] = 'Contexte';
+l10n::$locales['Function'] = 'Fonction';
+l10n::$locales['Warning: disable with care only, it could disturb the normal functions of plugins using stacker.'] = 'Attention : désactivez avec attention, cela peut perturber le fonctionnement normal des plugins utilisant Stacker';
+l10n::$locales['change'] = 'modifier';
+l10n::$locales['Manages a bunch of transforming functions that can modify the entries text before display'] = 'Gérer un tas de fonctions de transformation qui peuvent modifier le texte des entrées avant l\'affichage';
